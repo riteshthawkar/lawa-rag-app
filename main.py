4928b742e7f32fbcea642ce4fd8a1ff27390c7df
Ritesh Thawkar <riteshthawkar2003@gmail.com>
2025-02-08 12:01:37 +0400

    change run command

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-import uvicorn
-from app import app 
+# import os
+# import uvicorn
+# from app import app 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8080)
